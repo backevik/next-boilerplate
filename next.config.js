c89cf22292dd9ff789b945c7d4c3211044c39ed0
Andreas Bäckevik <a.backevik@gmail.com>
2020-10-22 11:09:34 +0200

    fixed prod build

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+
+    // Important: return the modified config
+    return config;
+  },
+};
