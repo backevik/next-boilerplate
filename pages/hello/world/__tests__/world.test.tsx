b32e0214cc74242f24e59f0832769b99b282d42d
Andreas Bäckevik <a.backevik@gmail.com>
2020-10-22 11:15:50 +0200

    remove test

--- pages/hello/world/__tests__/world.test.tsx
+++ pages/hello/world/__tests__/world.test.tsx
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import Home from '@pages/index';
+import World from '..';
 
 describe('World', () => {
   it('should render a hello world text', () => {
-    const { getByText } = render(<Home />);
+    const { getByText } = render(<World />);
 
-    expect(getByText('Get started by editing')).toBeInTheDocument();
+    expect(getByText('Hello world')).toBeInTheDocument();
   });
 });
